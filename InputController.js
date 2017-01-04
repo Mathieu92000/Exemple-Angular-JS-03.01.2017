e2ce595f8c3e4ff587565f99ebd8a519d6fd6738
Mathieu92000 <mathieugabillard@gmail.com>
2017-01-04 11:54:46 +0100

    2 eme Exercice 04.01.2017

--- InputController.js
+++ InputController.js
@@ -3,13 +3,9 @@ var app= angular.module('myFirstApp');
 app.controller('InputController',function($scope, ValueService) {
     $scope.value1="";
 
-    function send(){
-        ValueService.value=$scope.value1;
-    }
-
-    $scope.$watch($scope.value1,$scope.send);
-
-    $scope.send=function(){
+    $scope.send = function(){
         ValueService.value = $scope.value1;
     }
+
+    $scope.$watch('value1',$scope.send);
 });
--- ShowController.js
+++ ShowController.js
@@ -1,18 +1,6 @@
 var app= angular.module('myFirstApp');
 
-app.controller('InputController',function($scope, ValueService) {
-    $scope.value="";
-
-    $scope.$watch($scope.value1,$scope.send);
-
-    $scope.send=function(){
-        ValueService.value = $scope.value1;
-    }
-});
-
-var app= angular.module('MyFirstApp');
-
-app.controller('ShowController,function($scope'){
+app.controller('ShowController',function($scope, ValueService){
     $scope.value2="";
 
  $scope.get=function(){
